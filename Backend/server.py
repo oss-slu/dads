775276d7de37664b20c7b35704cef8c4f33bcee0
thomasmcg77 <66293154+thomasmcg77@users.noreply.github.com>
2023-04-04 19:40:02 -0500

    Fixed CORS issue

--- Backend/server.py
+++ Backend/server.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+import csv
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/data")
-def data():
-    return {"data": ["data1", "data2", "data3", "data4"]}
+def index():
+    with open('./static/test_data.csv', encoding='utf-8') as csv_file:
+        csvReader = csv.DictReader(csv_file)
+
+        jsonData = []
+
+        for row in csvReader:
+            print(row)
+            jsonData.append(row)
+    return jsonify(jsonData)
 
 
 if __name__ == "__main__":
