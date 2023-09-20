381af2ffa1db8faddd015b5a4d0d726138207ed2
Evan Richmond <evan.richmond@slu.edu>
2023-09-20 17:09:16 -0500

    About page template and react template cleanup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,7 +5,7 @@ import SystemDetails from './pages/SystemDetails'
 import Sidebar from "./components/Sidebar";
 import Tobpbar from "./components/Topbar"
 import Toolbar from '@mui/material/Toolbar';
-
+import AboutPage from './pages/AboutPage';
 
 function App() {
 
@@ -23,7 +23,7 @@ function App() {
         </>}
 
         <Routes>
-          <Route index element={<p style={{ marginLeft: drawerWidth }}> home page</p>} />
+          <Route index element={<AboutPage style={{ marginLeft: drawerWidth*2 }}/>} />
           <Route path="exploreSystems" element={<ExploreSystems width={drawerWidth} />} />
           <Route path="system/:label" element={<SystemDetails width={drawerWidth}/>}/>
         </Routes>
---
+++ Frontend/src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div style={{ marginLeft: 250 }}> {/*Drawer width is 200px fixed, added 50px padding*/}
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget augue at erat
+        efficitur mollis. Sed vel nunc vel velit feugiat rhoncus.
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
