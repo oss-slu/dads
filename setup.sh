755344d8c9d686ac2c38fc1cb1a4d03322a928fb
Evan Richmond <evan.richmond@slu.edu>
2023-09-28 15:23:35 -0500

    Initial setup file added/OS detection is functional

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Detect the user's operating system
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux detected
+    echo "Detected Linux OS" 
+    # Run appropriate commands for Linux
+    # Start Docker database
+    #docker-compose up -d database
+    # Start Python backend
+    cd Backend
+    python server.py &
+    # Start React frontend
+    cd ../Frontend
+    npm run start
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS detected 
+    echo "Detected macOS"
+    # Run appropriate commands for macOS
+    # Start Docker database
+    #docker-compose up -d database
+    # Start Python backend
+    cd Backend
+    python server.py &
+    # Start React frontend
+    cd ../Frontend
+    npm run start
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    # Windows detected
+    echo "Detected Windows OS"
+    # Run appropriate commands for Windows
+    # Start Docker database
+    #docker-compose up -d database
+    # Start Python backend
+    cd Backend
+    start cmd /c "python3 server.py"
+    # Start React frontend
+    cd ../Frontend
+    start cmd /c "npm run start"
+else
+    echo "Unsupported operating system"
+    exit 1
+fi
