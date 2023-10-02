96e6c7683b628bd4f7d34b575e4970dd0c21684a
Andrew <andrew.jelliss@slu.edu>
2023-10-02 14:53:36 -0500

    the connector is now a class and used as an object in the flask app.

--- Backend/PostgresConnector.py
+++ Backend/PostgresConnector.py
@@ -6,64 +6,66 @@ import psycopg2
 
 # gets all systems from the database
 
-def __init__(self):
-    self.connection = psycopg2.connect(
-            "dbname=dynamSystems user=postgres password=docker")
-    self.cursor = self.connection.cursor()
+class PostgresConnector:
+
+    def __init__(self):
+        self.connection = psycopg2.connect(
+                "dbname=dynamSystems user=postgres password=docker")
+        self.cursor = self.connection.cursor()
 
 
 
-def getAllSystems():
-    columns = '*'
-    sql = "SELECT " + columns + " FROM public.data"
-    self.cursor.execute(sql)
-    return self.cursor.fetchall()
+    def getAllSystems(self):
+        columns = '*'
+        sql = "SELECT " + columns + " FROM public.data"
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
 
 
 # gets a system identified by its label, input is string
-def getSystem(label):
-    columns = '*'
-    sql = "SELECT " + columns + " FROM public.data WHERE label = '" + label + "'"
-    self.cursor.execute(sql)
-    return self.cursor.fetchall()
+    def getSystem(self,label):
+        columns = '*'
+        sql = "SELECT " + columns + " FROM public.data WHERE label = '" + label + "'"
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
 
 
 # gets systems that match the passed in filters, input should be json object
-def getFilteredSystems(filters):
-    columns = 'label, N, degree, models_original_polys_val, base_field_latex'
-    whereText = buildWhereText(filters)
-    sql = "SELECT " + columns + " FROM public.data" + whereText
-    self.cursor.execute(sql)
-    return self.cursor.fetchall()
+    def getFilteredSystems(self,filters):
+        columns = 'label, N, degree, models_original_polys_val, base_field_latex'
+        whereText = buildWhereText(filters)
+        sql = "SELECT " + columns + " FROM public.data" + whereText
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
 
 
 # gets a subset of the systems identified by the labels, input should be json list
-def getSelectedSystems(labels):
-    labels = "(" + ", ".join(["'" + str(item) + "'" for item in labels]) + ")"
-    columns = '*'
-    sql = "SELECT " + columns + " FROM public.data WHERE label in " + labels
-    self.cursor.execute(sql)
-    return self.cursor.fetchall()
+    def getSelectedSystems(self,labels):
+        labels = "(" + ", ".join(["'" + str(item) + "'" for item in labels]) + ")"
+        columns = '*'
+        sql = "SELECT " + columns + " FROM public.data WHERE label in " + labels
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
 
 
 # function that builds the WHERE part of SQL query to filter the results
-def buildWhereText(filters):
+    def buildWhereText(self,filters):
     # remove empty filters
-    for filter in filters.copy():
-        if filters[filter] == []:
-            del filters[filter]
+        for filter in filters.copy():
+            if filters[filter] == []:
+                del filters[filter]
 
-    if len(filters) == 0:
-        return ""
+        if len(filters) == 0:
+            return ""
 
-    filterText = " WHERE "
+        filterText = " WHERE "
 
     # go through each non-empty filter and add it to the list for the WHERE clause
-    for index, filter in enumerate(filters):
-        filterText += filter + " in " + \
-            "(" + ', '.join(str(e) for e in filters[filter]) + ")"
+        for index, filter in enumerate(filters):
+            filterText += filter + " in " + \
+                    "(" + ', '.join(str(e) for e in filters[filter]) + ")"
 
-        if (index != len(filters)-1):
-            filterText += " AND "
+            if (index != len(filters)-1):
+                filterText += " AND "
 
-    return filterText
+        return filterText
