363fc5cc1f1dd762c615c44c2f6dffaed5035eae
Evan Richmond <evan.richmond@slu.edu>
2023-11-16 17:08:56 -0600

    Fixed formatting/imports

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
