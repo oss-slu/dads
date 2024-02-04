a360e79a0f8ed414362a7c88ee93cf154a0bef81
Evan Richmond <evan.richmond@slu.edu>
2024-02-04 15:39:51 -0600

    Functional test

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+import sys
+import os
+
+# This code will run before any tests are executed
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
--- tests/test_postgres_connector.py
+++ tests/test_postgres_connector.py
@@ -1,7 +1,9 @@
 import pytest
-from ..Backend.PostgresConnector import PostgresConnector
+from PostgresConnector import PostgresConnector
+from unittest.mock import patch
 
-def test_build_where_text():
+@patch('psycopg2.connect')
+def test_build_where_text(mock_connect):
     connector = PostgresConnector()
 
     filters = {
@@ -22,9 +24,8 @@ def test_build_where_text():
 
     # Call the buildWhereText method
     where_clause = connector.buildWhereText(filters)
-
     # Define the expected WHERE clause
-    expected_clause = " WHERE dimension IN (2) AND degree IN (3, 4) AND is_polynomial IN (True) AND is_Chebyshev IN (False) AND is_Newton IN (True) AND is_pcf IN (True) AND automorphism_group_cardinality = '5' AND base_field_label LIKE '%Q%' AND base_field_degree = '2'"
+    expected_clause = " WHERE dimension IN (2) AND degree IN (3, 4) AND is_polynomial IN (True) AND is_Chebyshev IN (False) AND is_Newton IN (True) AND is_pcf IN (True) AND CAST(automorphism_group_cardinality AS integer) IN (5) AND CAST(base_field_degree AS integer) IN (2)"
 
     # Assert that the output matches the expected string
     assert where_clause == expected_clause
