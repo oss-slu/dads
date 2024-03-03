5690df753bada71eda1b6d03644198c19ee5af58
Rolwes <m235370@academy.usna.edu>
2024-03-03 17:17:25 -0600

    test_postgres_completed

--- tests/test_postgres_connector.py
+++ tests/test_postgres_connector.py
@@ -7,29 +7,29 @@ def test_get_all_systems(mock_connect):
     mock_conn = mock_connect.return_value
     mock_cur = MagicMock()
     mock_conn.cursor.return_value = mock_cur
-    mock_cur.fetchall.return_value = [('System1',), ('System2',)]
+    mock_cur.fetchall.return_value = [("System1",), ("System2",)]
 
     connector = PostgresConnector()
     result = connector.getAllSystems()
 
     mock_cur.execute.assert_called_once_with("SELECT * FROM public.data")
     assert len(result) == 2
-    assert result[0][0] == 'System1'
-    assert result[1][0] == 'System2'
+    assert result[0][0] == "System1"
+    assert result[1][0] == "System2"
 
-@patch('psycopg2.connect')
+@patch("psycopg2.connect")
 def test_get_system(mock_connect):
     mock_conn = mock_connect.return_value
     mock_cur = MagicMock()
     mock_conn.cursor.return_value = mock_cur
-    mock_cur.fetchall.return_value = [('SpecificSystem',)]
+    mock_cur.fetchall.return_value = [("SpecificSystem",)]
 
     connector = PostgresConnector()
-    result = connector.getSystem('SpecificLabel')
+    result = connector.getSystem("SpecificLabel")
     sql = "SELECT * FROM public.data WHERE label = 'SpecificLabel'"
     mock_cur.execute.assert_called_once_with(sql)
     assert len(result) == 1
-    assert result[0][0] == 'SpecificSystem'
+    assert result[0][0] == "SpecificSystem"
 
 
 @patch('psycopg2.connect')
@@ -37,20 +37,20 @@ def test_get_selected_systems(mock_connect):
     mock_conn = mock_connect.return_value
     mock_cur = MagicMock()
     mock_conn.cursor.return_value = mock_cur
-    mock_cur.fetchall.return_value = [('SelectedSystem1',), ('SelectedSystem2',)]
+    mock_cur.fetchall.return_value = [("SelectedSystem1",), ("SelectedSystem2",)]
 
     connector = PostgresConnector()
-    labels = ['Label1', 'Label2']
+    labels = ["Label1", "Label2"]
     result = connector.getSelectedSystems(labels)
 
     labels_str = "('Label1', 'Label2')"
     sql = "SELECT * FROM public.data WHERE label in {labels_str}"
     mock_cur.execute.assert_called_once_with(sql)
     assert len(result) == 2
-    assert result[0][0] == 'SelectedSystem1'
-    assert result[1][0] == 'SelectedSystem2'
+    assert result[0][0] == "SelectedSystem1"
+    assert result[1][0] == "SelectedSystem2"
 
-@patch('psycopg2.connect')
+@patch("psycopg2.connect")
 def test_get_statistics(mock_connect):
     mock_conn = mock_connect.return_value
     mock_cur = MagicMock()
@@ -64,7 +64,7 @@ def test_get_statistics(mock_connect):
     ]
 
     connector = PostgresConnector()
-    filters = {'degree': [2], 'N': [4]}
+    filters = {"degree": [2], 'N': [4]}
     result = connector.getStatistics(filters)
 
     assert len(result) == 5
