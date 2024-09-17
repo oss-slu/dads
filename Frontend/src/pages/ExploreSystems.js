4c9dca2e41d609db211c13ada783bd16a3af6288
cr@ft!-c0der <syedomair0@gmail.com>
2024-09-17 15:55:33 -0500

    removed number input spinner

--- Frontend/src/pages/ExploreSystems.js
+++ Frontend/src/pages/ExploreSystems.js
@@ -489,28 +489,22 @@ function ExploreSystems() {
                                     <ul className="nested">
                                         <li>
                                             <input
-                                                type="number"
+                                                type="text"
                                                 style={textBoxStyle}
-                                                value={filters.rationalPreperiodicCardinality || ''}
-                                                onChange={(e) => handleTextChange("rationalPreperiodicCardinality", e.target.value)}
                                             />
                                             <label>Cardinality</label>
                                         </li>
                                         <li>
                                             <input
-                                                type="number"
+                                                type="text"
                                                 style={textBoxStyle}
-                                                value={filters.rationalPreperiodicComponents || ''}
-                                                onChange={(e) => handleTextChange("rationalPreperiodicComponents", e.target.value)}
                                             />
                                             <label>Number of Connected Components</label>
                                         </li>
                                         <li>
                                             <input
-                                                type="number"
+                                                type="text"
                                                 style={textBoxStyle}
-                                                value={filters.rationalPreperiodicLongestTail || ''}
-                                                onChange={(e) => handleTextChange("rationalPreperiodicLongestTail", e.target.value)}
                                             />
                                             <label>Longest Tail</label>
                                         </li>
