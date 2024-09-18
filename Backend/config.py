ddb49c73ba89e522b9af2a09369c01e413bf55ca
jwang-101 <justin.wang@slu.edu>
2024-09-18 15:02:58 -0500

    updated labels for web

--- Backend/config.py
+++ Backend/config.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
 
-def load_config(filename='database.ini', section='postgresql'):
+def load_config(filename='database.ini', section='postgresql_local'):
     parser = ConfigParser()
     parser.read(filename)
 
--- Backend/postgres_connector.py
+++ Backend/postgres_connector.py
@@ -35,7 +35,6 @@ class PostgresConnector:
             '1.' +
             data['sigma_one'] +
             '.' + data['sigma_two'] +
-            '.' + data['sigma_three'] +
             '.' + str(data['ordinal'])
         )
 
@@ -108,7 +107,7 @@ class PostgresConnector:
         #    label, dimension, degree, polynomials, field_label
 
         columns = (
-            'function_id, sigma_one, sigma_two, sigma_three, ordinal,'
+            'function_id, sigma_one, sigma_two, ordinal,'
             ' degree, (original_model).coeffs, base_field_label'
         )
         dims = filters['N']
