9279b3320a02f54b4591465165ec6800e6d5e55e
cr@ft!-c0der <syedomair0@gmail.com>
2024-10-09 14:55:20 -0500

    changes after review 2

--- Frontend/src/pages/FamilyDetails.js
+++ Frontend/src/pages/FamilyDetails.js
@@ -1,7 +1,89 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { get_family } from '../api/routes';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableRow, Paper } from '@mui/material';
+
+function FamilyDetailsTable({ family }) {
+  const formatModelCoeffs = (coeffs) => {
+    if (!Array.isArray(coeffs)) return 'N/A';
+    return coeffs.map(row => `[${row.join(', ')}]`).join(', ');
+  };
+
+  const formatCitations = (citations) => {
+    if (!Array.isArray(citations)) return 'N/A';
+    return citations.join(', ');
+  };
+
+  return (
+    <TableContainer className='table-component' component={Paper}>
+      <h3>Family Details</h3>
+      <Table aria-label="family details table">
+        <TableBody>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Family ID</b></TableCell>
+            <TableCell align="right">{family[0]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Name</b></TableCell>
+            <TableCell align="right">{family[1]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Degree</b></TableCell>
+            <TableCell align="right">{family[2]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Num Parameters</b></TableCell>
+            <TableCell align="right">{family[3]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Model Coeffs</b></TableCell>
+            <TableCell align="right">{formatModelCoeffs(family[4])}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Model Resultant</b></TableCell>
+            <TableCell align="right">{family[5]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Base Field Label</b></TableCell>
+            <TableCell align="right">{family[6]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Base Field Degree</b></TableCell>
+            <TableCell align="right">{family[7]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Sigma One</b></TableCell>
+            <TableCell align="right">{family[8]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Sigma Two</b></TableCell>
+            <TableCell align="right">{family[9]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Ordinal</b></TableCell>
+            <TableCell align="right">{family[10]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Citations</b></TableCell>
+            <TableCell align="right">{formatCitations(family[11])}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Is Polynomial</b></TableCell>
+            <TableCell align="right">{family[12] ? 'Yes' : 'No'}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Num Critical Points</b></TableCell>
+            <TableCell align="right">{family[13]}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell component="th" scope="row"><b>Automorphism Group Cardinality</b></TableCell>
+            <TableCell align="right">{family[14]}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 function FamilyDetails() {
   const [family, setFamily] = useState(null);
@@ -25,96 +107,15 @@ function FamilyDetails() {
     fetchFamily();
   }, [familyId]);
 
-  // Function to format the model_coeffs array
-  const formatModelCoeffs = (coeffs) => {
-    if (!Array.isArray(coeffs)) return 'N/A';
-    return coeffs.map(row => `[${row.join(', ')}]`).join(', ');
-  };
-
-  // Function to format the citations array
-  const formatCitations = (citations) => {
-    if (!Array.isArray(citations)) return 'N/A';
-    return citations.join(', ');
-  };
-
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
   if (!family) return <div>No family found with the given ID.</div>;
 
   return (
     <div className="family-details">
-      <br></br>
-      <br></br>
-      <br></br>
-      <h1>Family Details: {family[1]}</h1>
       <div className="info-container">
         <div className="row">
-          <TableContainer className='table-component' component={Paper}>
-            <Table aria-label="family details table">
-              <TableBody>
-                <TableRow>
-                  <TableCell><b>Family ID</b></TableCell>
-                  <TableCell>{family[0]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Name</b></TableCell>
-                  <TableCell>{family[1]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Degree</b></TableCell>
-                  <TableCell>{family[2]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Num Parameters</b></TableCell>
-                  <TableCell>{family[3]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Model Coeffs</b></TableCell>
-                  <TableCell>{formatModelCoeffs(family[4])}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Model Resultant</b></TableCell>
-                  <TableCell>{family[5]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Base Field Label</b></TableCell>
-                  <TableCell>{family[6]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Base Field Degree</b></TableCell>
-                  <TableCell>{family[7]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Sigma One</b></TableCell>
-                  <TableCell>{family[8]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Sigma Two</b></TableCell>
-                  <TableCell>{family[9]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Ordinal</b></TableCell>
-                  <TableCell>{family[10]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Citations</b></TableCell>
-                  <TableCell>{formatCitations(family[11])}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Is Polynomial</b></TableCell>
-                  <TableCell>{family[12] ? 'Yes' : 'No'}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Num Critical Points</b></TableCell>
-                  <TableCell>{family[13]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Automorphism Group Cardinality</b></TableCell>
-                  <TableCell>{family[14]}</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <FamilyDetailsTable family={family} />
         </div>
       </div>
     </div>
