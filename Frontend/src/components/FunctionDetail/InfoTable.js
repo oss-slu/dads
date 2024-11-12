5bf4682b47e7949e89e71e098357dab5757a303e
cr@ft!-c0der <syedomair0@gmail.com>
2024-11-11 22:43:51 -0600

    initial commit

--- Frontend/src/components/FunctionDetail/InfoTable.js
+++ Frontend/src/components/FunctionDetail/InfoTable.js
@@ -6,8 +6,15 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useNavigate } from 'react-router-dom';
 
 export default function InfoTable({ data }) {
+  const navigate = useNavigate();
+
+  const handleLinkClick = (selection) => {
+    navigate(`/family-details/${selection}`);
+  };
+
   return (
     <TableContainer className='table-component' component={Paper}>
       <Table aria-label="simple table">
@@ -18,6 +25,7 @@ export default function InfoTable({ data }) {
             <TableCell align="right"><b>Standard Model</b></TableCell>
             <TableCell align="right"><b>Degree</b></TableCell>
             <TableCell align="right"><b>Field of Definition</b></TableCell>
+            <TableCell align="right"><b>Family</b></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -27,6 +35,21 @@ export default function InfoTable({ data }) {
             <TableCell align="right">{data.display_model}</TableCell>
             <TableCell align="right">{data.degree}</TableCell>
             <TableCell align="right">{data.base_field_label}</TableCell>
+            <TableCell align="right">
+              {data.family && (
+                <button
+                  onClick={() => handleLinkClick(data.family)}
+                  style={{
+                    border: "None",
+                    color: "red",
+                    backgroundColor: "rgba(0, 0, 0, 0)",
+                    cursor: "pointer"
+                  }}
+                >
+                  {data.family}
+                </button>
+              )}
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
