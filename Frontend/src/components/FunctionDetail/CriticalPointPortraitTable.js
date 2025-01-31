0523b604db8ac8a7e88a8a832c3def067edfce51
John Doan <123352009+johndoans@users.noreply.github.com>
2025-01-31 05:13:28 +0000

    Fixed issue #184 hopefully

--- Frontend/src/components/FunctionDetail/CriticalPointPortraitTable.js
+++ Frontend/src/components/FunctionDetail/CriticalPointPortraitTable.js
@@ -5,9 +5,10 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import AdjacencyMatrix from '../FunctionDetail/AdjacencyMatrix'
+import Copy from '../FunctionDetail/Copy'
 
 export default function CriticalPointPortraitTable({ data }) {
-  console.log(JSON.stringify(data));
   const formatData = (key) => {
     const value = data[key];
   
@@ -26,39 +27,47 @@ export default function CriticalPointPortraitTable({ data }) {
       return '[]'; // Return '[]' for empty arrays
     }
   };
+  
   if (data.is_pcf == true) {
-  return (
-    <TableContainer className='table-component' component={Paper}>
-      <h3>Critical Point Portrait</h3>
-      <Table aria-label="simple table">
-        <TableBody>
-          <TableRow>
-            <TableCell component="th" scope="row"><b>Cardinality</b></TableCell>
-            <TableCell align="right">{data.cardinality || "N/A"}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row"><b>Size of Post Critical Set</b></TableCell>
-            <TableCell align="right">{data.positive_in_degree || "N/A"}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row"><b>As Directed Graph</b></TableCell>
-            <TableCell align="right">{"test"}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row"><b>Adjacency Matrix</b></TableCell>
-            <TableCell align="right">{"test"}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row"><b>Cycle Lengths</b></TableCell>
-            <TableCell align="right">{data.periodic_cycles || "N/A"}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row"><b>Component Sizes</b></TableCell>
-            <TableCell align="right">{data.preperiodic_components || "N/A"}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
+    return (
+      <TableContainer className='table-component' component={Paper}>
+        <h3>Critical Point Portrait</h3>
+        <Table aria-label="simple table">
+          <TableBody>
+            <TableRow>
+              <TableCell component="th" scope="row"><b>Cardinality</b></TableCell>
+              <TableCell align="right">{data.cardinality || "N/A"}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row"><b>Size of Post Critical Set</b></TableCell>
+              <TableCell align="right">{data.positive_in_degree || "N/A"}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row"><b>As Directed Graph</b></TableCell>
+              <TableCell align="right">
+                  <Copy edges={formatData("edges")} type={2} />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row"><b>Adjacency Matrix</b></TableCell>
+              <TableCell align="right">
+                  <Copy edges={formatData("edges")} type={1} />
+                  <br>
+                  </br>
+                  <AdjacencyMatrix modalTitle={"Adjacency Matrix"} edges={formatData("edges")} />
+                </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row"><b>Cycle Lengths</b></TableCell>
+              <TableCell align="right">{data.periodic_cycles || "N/A"}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell component="th" scope="row"><b>Component Sizes</b></TableCell>
+              <TableCell align="right">{data.preperiodic_components || "N/A"}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+ }
 }
