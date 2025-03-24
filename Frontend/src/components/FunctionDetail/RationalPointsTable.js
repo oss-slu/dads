66690a59ef0ba021ff49c6d23225d0b75095b674
unknown <bradleyjoe123@gmail.com>
2025-03-24 13:05:43 -0500

    Fixed issue #209: Reformat rational points table

--- Frontend/src/components/FunctionDetail/RationalPointsTable.js
+++ Frontend/src/components/FunctionDetail/RationalPointsTable.js
@@ -4,37 +4,29 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
-import { get_rational_periodic_data, get_label } from '../../api/routes'; // Import API functions
+import { get_rational_periodic_data, get_label } from '../../api/routes';
 
 const ScrollableTableContainer = styled(TableContainer)({
-  maxHeight: '400px', // Enables scrolling
+  maxHeight: '400px',
   overflowY: 'auto'
-});
-
-const EntryContainer = styled('div')({
-  border: '1px solid #ccc',
-  borderRadius: '8px',
-  padding: '10px',
-  marginBottom: '10px',
-  backgroundColor: '#f9f9f9'
 });
 
 export default function RationalPointsTable({ data }) {
   const [rationalData, setRationalData] = useState([]);
   const [label, setLabel] = useState('');
 
-  // Extract function_id from data
   const functionId = data?.function_id;
 
-  // Fetch rational preperiodic data and label on mount
   useEffect(() => {
     if (functionId) {
       get_rational_periodic_data(functionId)
         .then(response => {
-          setRationalData(response.data); // Store the array of results
+          console.log("Rational Periodic Data Response:", response.data);
+          setRationalData(response.data);
         })
         .catch(error => {
           console.error("Error fetching rational periodic data:", error);
@@ -56,38 +48,38 @@ export default function RationalPointsTable({ data }) {
     <>
       <h3>Rational Points Table</h3>
       <ScrollableTableContainer component={Paper}>
-        {rationalData.map((item, index) => (
-          <EntryContainer key={index}>
-            <Table size="small">
-              <TableBody>
-                <TableRow>
-                  <TableCell><b>Label</b></TableCell>
-                  <TableCell>{label}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Function ID</b></TableCell>
-                  <TableCell>{item[1]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Field Label</b></TableCell>
-                  <TableCell>{item[2]}</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Rational Preperiodic Points</b></TableCell>
-                  <TableCell>
-                    {item[3].map((point, idx) => (
-                      <div key={idx}>{`(${point[0]}, ${point[1]})`}</div>
-                    ))}
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell><b>Graph ID</b></TableCell>
-                  <TableCell>{item[4]}</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </EntryContainer>
-        ))}
+        <Table size="small" stickyHeader>
+          <TableHead>
+            <TableRow>
+              <TableCell><b>Field Label</b></TableCell>
+              <TableCell><b>Cardinality</b></TableCell>
+              <TableCell><b>As Directed Graph</b></TableCell>
+              <TableCell><b>Adjacency Matrix</b></TableCell>
+              <TableCell><b>Cycle Lengths</b></TableCell>
+              <TableCell><b>Component Sizes</b></TableCell>
+              <TableCell><b>Rational Preperiodic Points</b></TableCell>
+              <TableCell><b>Longest Tail</b></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rationalData.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>{item[2]}</TableCell> {/* Field Label */}
+                <TableCell>{item[1]}</TableCell> {/* Cardinality */}
+                <TableCell>{item[6]}</TableCell> {/* As Directed Graph */}
+                <TableCell>{item[7]}</TableCell> {/* Adjacency Matrix */}
+                <TableCell>{item[8]}</TableCell> {/* Cycle Lengths */}
+                <TableCell>{item[9]}</TableCell> {/* Component Sizes */}
+                <TableCell>
+                  {item[3].map((point, idx) => (
+                    <div key={idx}>{`(${point[0]}, ${point[1]})`}</div>
+                  ))}
+                </TableCell>
+                <TableCell>{item[4]}</TableCell> {/* Longest Tail */}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </ScrollableTableContainer>
     </>
   );
